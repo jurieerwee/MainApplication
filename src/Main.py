3b445c919de28b8819661b6171a654380e4f7826
Jurie <16491424@sun.ac.za>
2015-11-25 17:52:58 +0200

    Lastest Main

--- src/Main.py
+++ src/Main.py
@@ -10,9 +10,29 @@ if __name__ == '__main__':
 from Comms import *
 import Control
 import threading
+import socket
+import time
+import sys
 
-rigComms = RigComms('172.168.0.63',5000)
-uiComms = UIComms('172.168.0.63',5001)
+try:
+	rigComms = RigComms('172.168.0.63',5000)
+except (socket.error):
+	print('rigComms init failed')
+	exit()
+	
+# class UIComms(object):
+# 	def sendWarning(self,msg):
+# 		print (msg)
+# 		
+# 	def sendError(self,msg):
+# 		print(msg)
+# 	
+# uiComms = UIComms()
+try:	
+	uiComms = UIComms('172.168.0.63',5001)
+except (socket.error):
+	print('uiComms init failed')
+	sys.exit()
 
 rigComms.activateRigToUI(uiComms)
 uiComms.activateUItoRig(rigComms)
@@ -26,9 +46,25 @@ t_recv.start()
 t_trans = threading.Thread(target= rigComms.transmit)
 t_trans.start()
 
+
+
+rigComms.sendCmd({'type':'setCMD','instr':'activateUpdate'})
+time.sleep(2)
+
+i =0
+while(not rigComms.getStatus() and i <10):
+	time.sleep(0.5)
+	i +=1
+
+if(i==10):
+	print('No status...',rigComms.getStatus() )
+	exit()
+
+print(rigComms.getStatus())
+
 ctrl = Control.Control(rigComms,uiComms)
 
-
+ctrl.controlLoop()
 
 
 rigComms.terminateComms()
